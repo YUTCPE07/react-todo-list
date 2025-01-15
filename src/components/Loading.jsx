5c068971f221ad9ad01d1fb97d616fecf16a1c1d
YUTCPE07 <teerapat-yut@hotmail.com>
2025-01-15 13:55:19 +0700

    Create Loading.jsx

---
+++ src/components/Loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <>
+            <div className='text-center my-32 text-5xl text-purple-700 drop-shadow'>Loading...</div>
+        </>
+    )
+}
